e5fca0538c1127786f1c8101d1f1a5cc3f8caad3
langermank <18661030+langermank@users.noreply.github.com>
2023-02-10 12:47:02 -0800

    awesome theme
    Co-authored-by: Josh Black <joshblack@users.noreply.github.com>

--- docs/storybook/.storybook/main.js
+++ docs/storybook/.storybook/main.js
@@ -2,4 +2,11 @@ module.exports = {
   stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
   framework: '@storybook/react',
+  refs: {
+    'design-system': {
+      title: 'Primer React',
+      url: 'https://primer.style/react/storybook/',
+      expanded: false, // optional, true by default
+    },
+  },
 }
